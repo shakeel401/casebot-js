d772d4628b43a8c6bcbdf6f561a94deb65e10182
shakeel401 <146809696+shakeel401@users.noreply.github.com>
2025-08-08 02:16:44 -0700

    Update filter.py

--- netlify/functions/filter.py
+++ netlify/functions/filter.py
@@ -1,15 +1,18 @@
-def is_question_valid(user_input: str) -> bool:
-    banned_keywords = [
-        "joke", "funny", "lol", "haha", "laugh",
-        "crush", "kiss", "hug", "flirt",
-        "dating","sex",
-        "boyfriend", "girlfriend",
-        "do you love me", "tell me a joke", "marry me", "i love you",
-        "chat with me", "best friend", "are you single", "romantic", "cute"
-    ]
+export default function isQuestionValid(userInput) {
+  const bannedKeywords = [
+    "joke", "funny", "lol", "haha", "laugh",
+    "crush", "kiss", "hug", "flirt",
+    "dating", "sex",
+    "boyfriend", "girlfriend",
+    "do you love me", "tell me a joke", "marry me", "i love you",
+    "chat with me", "best friend", "are you single", "romantic", "cute"
+  ];
 
-    user_input_lower = user_input.lower()
-    for word in banned_keywords:
-        if word in user_input_lower:
-            return False
-    return True
+  const userInputLower = userInput.toLowerCase();
+  for (const word of bannedKeywords) {
+    if (userInputLower.includes(word)) {
+      return false;
+    }
+  }
+  return true;
+}
